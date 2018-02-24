943e61a6d79d9a8b93b070a19a9b3bcc27c20abb
Jesse <jesse@guojiang.tv>
2018-02-24 10:25:29 +0800

    add python

---
+++ Python/pic1.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def getHtmlurl(url): #通过网址获取html内容
+    try:
+        r=requests.get(url)
+        r.raise_for_status()
+        r.encoding=r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+def getpic(html): #获取图片地址并下载
+    soup =BeautifulSoup(html,'html.parser')
+    # all_img=soup.find('ul',class_='pli').find_all('img')
+    all_img=soup.find_all('img')
+    for img in all_img:
+        img_url=img['src']
+        root='D:/pic/'
+        path = root + img_url.split('/')[-1].split('?')[0]
+        try:                              #创建或判断路径图片是否存在并下载
+            if not os.path.exists(root):
+                os.mkdir(root)
+            if not os.path.exists(path):
+                r = requests.get(img_url)
+                with open(path, 'wb') as f:
+                    f.write(r.content)
+                    f.close()
+                    print("文件保存成功")
+            else:
+                print("文件已存在")
+        except:
+            print("爬取失败")
+
+
+
+def main():
+    url='http://www.ivsky.com/bizhi/yourname_v39947/'
+    html=(getHtmlurl(url))
+    # print(html)
+    print(getpic(html))
+main()
---
+++ Python/test2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-02-23 14:56:31
+# @Author  : Jesse (jesse152@163.com)
+# @Link    : http://example.org
+# @Version : $Id$
+import MySQLdb

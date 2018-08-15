0b2621f821a1752f983a1144f61a4cf1f9735da5
Jesse <jesse@guojiang.tv>
2018-08-15 18:44:45 +0800

    add qrcode

---
+++ Wechat/qrcode/pages/index/index.js
@@ -0,0 +1,42 @@
+//index.js
+//获取应用实例
+
+import QR from '../../utils/qr.js';
+Page({
+  data: {
+    url:'http://www.baidu.com'
+  },
+  onLoad:function(options){
+    let size = this.getCanvasSize();
+    let url=this.data.url;
+    this.createQRcode(url,'mycanvas',size.w,size.h);
+  },
+  getCanvasSize(){
+    let size = {};
+    let res=wx.getSystemInfoSync();
+    let scale = 686/750;
+    let width = res.windowWidth*scale;
+    let height = width;
+    size.w = width;
+    size.h = height;
+    return size;
+  },
+  createQRcode(url,canvasId,canvasW,canvasH){
+    console.log(QR)
+    // QR.api.draw(url, canvasId, canvasW, canvasH)
+  },
+  fromSubmit(e){
+    let url=e.detail.value.url || this.data.url;
+    wx.showToast({
+      title:'生成中，请稍等',
+      icon:'loading',
+      duration:2000
+    })
+    let timer=setTimeout(()=>{
+      let size = this.getCanvasSize();
+      this.createQRcode(url, 'mycanvas', size.w, size.h);
+      wx.hideToast();
+      clearTimeout(timer);
+    },2000)
+  }
+})

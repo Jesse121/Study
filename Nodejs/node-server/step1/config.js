fa0de3d72d8da3f9e40dc79973a4a3a12b47aea3
Jesse <jesse@guojiang.tv>
2018-10-22 11:30:14 +0800

    electron

--- Nodejs/node-server/step1/config.js
+++ Nodejs/node-server/step1/config.js
@@ -2,3 +2,9 @@ exports.Expires = {
     fileMatch: /^(gif|png|jpg|js|css)$/gi,
     maxAge: 60 * 60 * 24 * 365
 };
+exports.Compress = {
+    match: /css|js|html/gi
+};
+exports.Welcome = {
+    file: "index.html"
+};

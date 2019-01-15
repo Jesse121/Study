366326eb58ee7eeb4b7f79c949b415d3ff269fd5
Jesse <jesse@guojiang.tv>
2019-01-15 16:39:15 +0800

    remvoe-node

--- Nodejs/Blog/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: 3000, //端口号
-    session: {
-        secret: 'Blog',
-        key: 'Blog',
-        maxAge: 6000000
-    },
-    mongodb: 'mongodb://localhost/Blog' //连接本地数据库
-};
--- Nodejs/node.js-the-definitive-guide/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-var server = http.createServer()
-
-server.on('request',function (req,res) {
-    console.log(req.url)
-    res.end()
-
-})
-server.listen(3000,'127.0.0.2')
